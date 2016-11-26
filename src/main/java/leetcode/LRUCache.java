5d6cbf3b284edbd030f355bcf90fd083048e0443
fyl <fuyulai@gmail.com>
2016-11-25 17:13:44 -0800

    c

--- src/main/java/leetcode/LRUCache.java
+++ src/main/java/leetcode/LRUCache.java
@@ -1,57 +1,121 @@
 package leetcode;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 
 public class LRUCache {
     private int capacity;
-    private LinkedList<Node> doublyLinkedList;
+    private Node head;
+    private Node tail;
     private HashMap<Integer, Node> map = new HashMap();
     private int vol;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        doublyLinkedList = new LinkedList<Node>();
+
     }
 
     public int get(int key) {
-        Node node = map.get(key);
-        if (node == null)
-            return -1;
-        int val = node.val;
-        doublyLinkedList.remove(node);
-        doublyLinkedList.addLast(node);
-        return val;
+        if (!map.containsKey(key)) {
+            return  -1;
+        }
+
+        Node n = map.get(key);
+
+        Node prev = n.prev;
+        Node next = n.next;
+
+        if (this.head != n) {
+            if (this.tail == n) {
+                prev.next = null;
+                this.tail = prev;
+            } else {
+                prev.next = next;
+                next.prev = prev;
+            }
+
+            addToHead(n);
+        }
+
+        return head.val;
     }
 
     public void set(int key, int value) {
-        if (map.containsKey(key)) {
-            doublyLinkedList.remove(map.get(key));
-            vol --;
+        if (capacity < 1) {
+            return;
         }
 
-        Node node = new Node(value);
-        doublyLinkedList.addLast(node);
-        map.put(key, node);
-        vol ++;
+        if (this.map.containsKey(key)) {
+            Node n = this.map.get(key);
+            n.val = value;
 
-        if (vol > capacity) {
-            Node head = doublyLinkedList.removeFirst();
-            Integer keyToRemove = null;
-            for (int k : map.keySet()) {
-                if (map.get(k).equals(head))
-                    keyToRemove = k;
+            Node prev = n.prev;
+            Node next = n.next;
+
+            if (this.head != n) {
+
+                if (this.tail == n) {
+                    prev.next = null;
+                    this.tail = prev;
+                } else {
+                    prev.next = next;
+                    next.prev = prev;
+
+
+                }
+                addToHead(n);
+                map.put(key, head);
+
             }
-            map.remove(keyToRemove);
-            vol --;
+        } else {
+            Node n = new Node(key, value);
+
+            if (this.vol < this.capacity) {
+                this.map.put(key, n);
+                this.vol ++;
+                addToHead(n);
+            } else if (this.vol == this.capacity){
+                if (tail == head) {
+                }
+
+                Node oldTail = this.tail;
+                this.tail = oldTail.prev;
+                if (this.tail != null) {
+                    this.tail.next = null;
+                }
+                this.map.remove(oldTail.key);
+
+                addToHead(n);
+            } else {
+                //cannot happen
+            }
+
+            map.put(key, head);
+        }
+    }
+
+    private void addToHead(Node n) {
+        Node oldHead = head;
+        if (oldHead == null) {
+            head = n;
+            tail = n;
+        } else {
+            head = n;
+            oldHead.prev = head;
+            head.prev = null;
+            head.next = oldHead;
         }
 
     }
 
     public class Node {
         public int val;
-        public Node(int val) {
+        public int key;
+        public Node next;
+        public Node prev;
+
+        public Node(int key, int val) {
             this.val = val;
+            this.key=key;
         }
     }
 
