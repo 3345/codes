7bac82f9fa68965ce4660761e10bb64da4f0ef65
anom <anom@gmail.com>
2016-12-17 05:37:27 -0800

    c

--- src/main/java/leetcode/SparseMatrixMultiplication.java
+++ src/main/java/leetcode/SparseMatrixMultiplication.java
@@ -10,6 +10,7 @@ public class SparseMatrixMultiplication {
     public int[][] multiply(int[][] a, int[][] b) {
         Map<Integer, Map<Integer, Integer>> mapA = new HashMap<>();
         Map<Integer, Map<Integer, Integer>> mapB = new HashMap<>();
+        int[][] c = new int[a.length][b[0].length];
 
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[0].length; j++) {
@@ -24,5 +25,21 @@ public class SparseMatrixMultiplication {
                 }
             }
         }
+
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                if (b[i][j] != 0) {
+                    if (mapB.containsKey(i)) {
+                        mapB.get(i).put(j, b[i][j]);
+                    } else {
+                        Map<Integer, Integer> m = new HashMap<>();
+                        m.put(j, b[i][j]);
+                        mapB.put(i, m);
+                    }
+                }
+            }
+        }
+
+
     }
 }
