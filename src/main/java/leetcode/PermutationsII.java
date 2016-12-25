e604b1723bef4291265b3092dc93f6fa6359f1a3
fyl <fuyulai@gmail.com>
2016-12-25 01:45:54 -0800

    c

--- src/main/java/leetcode/PermutationsII.java
+++ src/main/java/leetcode/PermutationsII.java
@@ -14,46 +14,24 @@ import java.util.*;
 // wrong!!!
 public class PermutationsII {
     public List<List<Integer>> permuteUnique(int[] num) {
-        if (num.length == 0)
-            return null;
-        List<Integer> numList = new ArrayList();
-        for (int i = 0; i < num.length; i++) {
-            numList.add(num[i]);
-        }
-        Set<Integer> visited = new HashSet<Integer>();
-        return solve(numList, visited);
-
-    }
-
-    public List<List<Integer>> solve(List<Integer> num, Set<Integer> visited) {
-        if (num.size() == 1) {
-            List<List<Integer>> result = new ArrayList<List<Integer>>();
-            result.add(new ArrayList<Integer>(Arrays.asList(num.get(0))));
-            visited.add(num.get(0));
-            return result;
+        if (num == null || num.length == 0) {
+            return new ArrayList<>();
         }
 
-        int rightMost = num.get(num.size() - 1);
+        List<LinkedList<Integer>> list = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
 
-
-        List<List<Integer>> lists = solve(num.subList(0, num.size() - 1), visited);
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-
-        for (List<Integer> list : lists) {
-            if (!visited.contains(rightMost)) {
-                for (int i = 0; i < list.size() + 1; i++) {
-                    ArrayList<Integer> newList = new ArrayList<Integer>(list);
-                    newList.add(i, rightMost);
-                    result.add(newList);
-                }
-            } else {
-                list.add(rightMost);
-                result.add(list);
+        linkedList.add(num[0]);
+        for (int i = 1; i < num.length; i++) {
+            for(LinkedList<Integer> ll : list) {
+                list.remove(ll);
             }
         }
-        visited.add(rightMost);
-        return result;
+
     }
+
+
+
 
     @Test
     public void t1() {
