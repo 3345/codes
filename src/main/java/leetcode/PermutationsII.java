a3edaa0ec45ee9cfc045b79e41eda7b20277e1c9
yulai <yulai@yahoo-inc.com>
2016-12-26 02:24:23 -0800

    c

--- src/main/java/leetcode/PermutationsII.java
+++ src/main/java/leetcode/PermutationsII.java
@@ -1,8 +1,11 @@
 package leetcode;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-import java.util.*;
+import org.junit.Test;
 
 /**
  Given a collection of numbers that might contain duplicates, return all possible unique permutations.
@@ -14,20 +17,54 @@ import java.util.*;
 // wrong!!!
 public class PermutationsII {
     public List<List<Integer>> permuteUnique(int[] num) {
-        if (num == null || num.length == 0) {
-            return new ArrayList<>();
+
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < num.length; i++) {
+           list.add(num[i]);
         }
 
-        List<LinkedList<Integer>> list = new ArrayList<>();
-        LinkedList<Integer> linkedList = new LinkedList<>();
+        return solve(list);
+    }
 
-        linkedList.add(num[0]);
-        for (int i = 1; i < num.length; i++) {
-            for(LinkedList<Integer> ll : list) {
-                list.remove(ll);
+    public List<List<Integer>> solve(List<Integer> list) {
+        if (list.size() == 0) {
+            List<List<Integer>> l = new ArrayList<>();
+            List<Integer> ll = new ArrayList<>();
+            l.add(ll);
+            return l;
+        }
+        if (list.size() == 1) {
+            List<List<Integer>> l = new ArrayList<>();
+            List<Integer> ll = new ArrayList<>();
+            ll.add(list.get(0));
+            l.add(ll);
+            return l;
+
+        }
+
+        Set<Integer> visited = new HashSet<>();
+
+        int len = list.size();
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < len; i ++) {
+            int removed = list.get(i);
+            list.remove(i);
+            if (!visited.contains(removed)) {
+                visited.add(removed);
+
+                List<List<Integer>> subAnswer = solve(list);
+                for (List<Integer> l : subAnswer) {
+                    l.add(0, removed);
+                }
+
+                ans.addAll(subAnswer);
+
             }
+            list.add(i ,removed);
         }
 
+        return ans;
     }
 
 
@@ -36,7 +73,7 @@ public class PermutationsII {
     @Test
     public void t1() {
         //1,1,0,0,1,-1,-1,1
-        int[] arr = {1,1,1,0,0,0};
+        int[] arr = {1,1,2};
         List l = permuteUnique(arr);
         System.out.println(l);
     }
