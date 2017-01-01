d4c0f4133dc47b172cdcb6567d1dba766461056c
fyl <fuyulai@gmail.com>
2017-01-01 04:25:23 -0800

    c

--- src/main/java/leetcode/IntegerToEnglishWords.java
+++ src/main/java/leetcode/IntegerToEnglishWords.java
@@ -7,7 +7,10 @@ import java.util.Map;
  * Created by fuyul on 12/31/2016.
  */
 public class IntegerToEnglishWords {
-    Map
+    Map<Integer, String> numMap = new HashMap<>();
+    Map<Integer, String> tenthMap = new HashMap<>();
+    Map<Integer, String> tMap = new HashMap<>();
+
 
     public String numberToWords(int num) {
         Map<Integer, String> numMap = new HashMap<>();
@@ -21,7 +24,6 @@ public class IntegerToEnglishWords {
         numMap.put(8, "Eight");
         numMap.put(9, "Nine");
 
-        Map<Integer, String> tenthMap = new HashMap<>();
         tenthMap.put(10, "Ten");
         tenthMap.put(11, "Eleven");
         tenthMap.put(12, "Twelve");
@@ -32,6 +34,15 @@ public class IntegerToEnglishWords {
         tenthMap.put(17, "Seventeen");
         tenthMap.put(18, "Eighteen");
         tenthMap.put(19, "Nineteen");
+
+        tMap.put(2, "Twenty");
+        tMap.put(3, "Thirty");
+        tMap.put(4, "Forty");
+        tMap.put(5, "Fifty");
+        tMap.put(6, "Sixty");
+        tMap.put(7, "Seventy");
+        tMap.put(8, "Eight");
+        tMap.put(9, "Ninety");
 
         //2,147,483,647
         int g = num / 1000000000;
@@ -48,19 +59,54 @@ public class IntegerToEnglishWords {
         }
 
         int m = num / 1000000;
+
+        if (m != 0) {
+            sb.append(" ");
+            convertThreeDigits(m, sb);
+            sb.append(" Million");
+            num -= m * 1000000;
+        }
+
+        int k = num / 1000;
+
+        if (k != 0) {
+            sb.append(" ");
+            convertThreeDigits(k, sb);
+            sb.append(" Thousand");
+            num -= k *  1000;
+        }
+
+        if ( num != 0) {
+            convertThreeDigits(num, sb);
+        }
+
+        return sb.toString();
     }
 
-    public void convertThreeDigits(int num, StringBuilder sb, Map<Integer, String> numMap) {
+    public void convertThreeDigits(int num, StringBuilder sb) {
         int hundred = num / 100;
+        boolean hasH = false;
         if (hundred != 0) {
             sb.append(numMap.get(hundred));
             num -= 100 * hundred;
+            hasH = true;
         }
 
         int tenth = num / 10;
 
         if (tenth == 1) {
-
+            sb.append(tenthMap.get(num));
+        } else if (tenth == 0) {
+            if (num > 0) {
+                if (hasH) {
+                    sb.append("And ");
+                }
+                sb.append(numMap.get(num));
+            }
+        } else {
+            sb.append(tMap.get(tenth));
+            sb.append(" ");
+            sb.append(numMap.get(num - tenth * 10));
         }
     }
 }
