f2f71f5f6b3e84db68a2b311a37693b336ecdeb0
anom <anom@gmail.com>
2017-01-05 01:00:12 -0800

    c

--- src/main/java/leetcode/GroupAnagrams.java
+++ src/main/java/leetcode/GroupAnagrams.java
@@ -9,96 +9,29 @@ import java.util.*;
  */
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<Id, List<String>> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        List<List<String>> lists = new ArrayList<>();
 
-        for (int i = 0; i < strs.length; i ++) {
-            String s = strs[i];
-            Id id = new Id(s);
-
-            if (map.containsKey(id)) {
-                map.get(id).add(s);
-            } else {
-                List<String> list = new ArrayList<>();
-                list.add(s);
-                map.put(id, list);
-
-            }
+        if (strs == null) {
+            return lists;
         }
 
-        List<List<String>> ret = new ArrayList<>();
-        for (Id key : map.keySet()) {
-            ret.add(map.get(key));
-        }
-
-        return ret;
-
-    }
-
-    public class Id {
-        Map<Character, Integer> map = new HashMap<>();
-        int len = 0;
-
-        public Id(String s) {
+        for (String s : strs) {
             char[] a = s.toCharArray();
+            Arrays.sort(a);
 
-            len = a.length;
-
-            for (int i = 0; i < a.length; i++) {
-                char c = a[i];
-                if (map.containsKey(c)) {
-                    map.put(c, map.get(c) + 1);
-                } else {
-                    map.put(c, 1);
-                }
+            String id = new String(a);
+            if (map.containsKey(id)) {
+                lists.get(map.get(id)).add(s);
+            } else {
+                map.put(id, map.size());
+                lists.add(new ArrayList<>(Arrays.asList(s)));
             }
         }
 
-
-
-        @Override
-        public int hashCode() {
-            int result = map != null ? map.hashCode() : 0;
-            result = 31 * result + len;
-            return result;
-        }
-
-
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof Id)) {
-                return false;
-            }
-
-            Id that = (Id) o;
-
-            if (that.len != this.len) {
-                return false;
-            }
-
-            Map<Character, Integer> thatMap = that.map;
-
-            for (Map.Entry<Character, Integer> entry : this.map.entrySet()) {
-                Character k = entry.getKey();
-                Integer v = entry.getValue();
-
-                if (!thatMap.containsKey(k)) {
-                    return false;
-                } else {
-                    if (!v.equals(thatMap.get(k))) {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
-        }
+        return lists;
 
     }
 
-    @Test
-    public void t0() {
-        String[] input = {"", ""};
 
-        groupAnagrams(input);
-    }
 }
