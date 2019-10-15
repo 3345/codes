bc1bc5d54dd151ec5afe97849dca6e2d9ecda621
anom <anom@gmail.com>
2019-10-15 01:01:19 -0700

    c

--- src/main/java/leetcode/CherryPickup.java
+++ src/main/java/leetcode/CherryPickup.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 
 /**
@@ -12,7 +14,11 @@ public class CherryPickup {
         this.grid = grid;
         int n = grid.length;
         mem = new int[n][n][n];
-        Arrays.fill(mem, -2);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Arrays.fill(mem[i][j], -2);
+            }
+        }
         int max = -1;
         for (int i = 0; i < n; i++) {
             max = Math.max(dp(n-1, n-1, i), max);
@@ -23,14 +29,14 @@ public class CherryPickup {
 
     // x1+y1=x2+y2 -> y2=x1+y1-x2
     private int dp(int x1, int y1, int x2) {
+        int y2 = x1+y1-x2;
+
         if (this.mem[x1][y1][x2] > -2) {
             return this.mem[x1][y1][x2];
         }
-        int w = grid[0].length;
-        int h = grid.length;
-        int y2 = x1+y1-x2;
 
-        if (x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0) {
+
+        if (x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0 || y2 >= this.grid.length) {
             return -1;
         }
 
@@ -48,5 +54,11 @@ public class CherryPickup {
         }
         this.mem[x1][y1][x2] = max;
         return max;
+    }
+
+    @Test
+    public void test() {
+        int[][] in = new int[][]{{0,1,-1},{1,0,-1},{1,1,1}};
+        cherryPickup(in);
     }
 }
